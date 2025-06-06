033e77cd7fc7435b76d3ce631edf2b45696bfa1a
Ireaneus <bwdave@protonmail.com>
2025-06-06 20:36:39 +0000

    working with site variable added to astro.config.mjs and header.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://ireaneus.github.io/LivingWaters/',
 	base: "/LivingWaters/",
 	integrations: [mdx(), sitemap()],
 });
