19a31ec8a624923c63925cf16455a3272950435a
Ireaneus <bwdave@protonmail.com>
2025-06-06 20:51:30 +0000

    updated to use import of BASE

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+export const BASE = '/LivingWaters/';
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://ireaneus.github.io/LivingWaters/',
