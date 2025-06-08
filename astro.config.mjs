b172c41a0ced57c7e4c20da67fca7960824b7312
Ireaneus <bwdave@protonmail.com>
2025-06-08 20:27:47 +0000

    Initial Astro blog with MDX and GitHub Pages workflow

---
+++ astro.config.mjs
@@ -0,0 +1,29 @@
+import { defineConfig } from 'astro/config'
+import svelte from '@astrojs/svelte'
+import mdx from '@astrojs/mdx'
+import remarkGfm from 'remark-gfm'
+import remarkSmartypants from 'remark-smartypants'
+import rehypeExternalLinks from 'rehype-external-links'
+import sitemap from '@astrojs/sitemap';
+
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://ireaneus.github.io/LivingWaters/',
+  base: '/LivingWaters/',
+  integrations: [mdx(), svelte(), sitemap()],
+  markdown: {
+    shikiConfig: {
+      theme: 'nord',
+    },
+    remarkPlugins: [remarkGfm, remarkSmartypants],
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: '_blank',
+        },
+      ],
+    ],
+  },
+})
